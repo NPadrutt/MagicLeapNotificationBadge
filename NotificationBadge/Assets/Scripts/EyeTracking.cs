3f09e8351e8ff02e86c3d78ac4c5578c6c2e0be5
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-04-30 09:54:22 +0200

    add eye tracking

---
+++ NotificationBadge/Assets/Scripts/EyeTracking.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class EyeTracking : MonoBehaviour {
+    public Material FocusedMaterial, NonFocusedMaterial;
+    public GameObject DetailObjectToOpen;
+
+    [Tooltip("Specifies the target we will orient to. If no target is specified, the main camera will be used.")]
+    private Transform targetTransform;
+    public Transform TargetTransform {
+        get { return targetTransform; }
+        set { targetTransform = value; }
+    }
+
+    private Vector3 heading;
+    private MeshRenderer meshRenderer;
+    private bool isDetailOpen;
+
+
+
+    // Start is called before the first frame update
+    void Start() {
+        if (DetailObjectToOpen != null) {
+            DetailObjectToOpen.SetActive(false);
+        }
+
+        if (TargetTransform == null) {
+            TargetTransform = CameraCache.Main.transform;
+        }
+
+        MLEyes.Start();
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    private void OnDisable() {
+        Debug.Log("Eye Tracking Disabled");
+        MLEyes.Stop();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!MLEyes.IsStarted) {
+            MLEyes.Start();
+            Debug.Log("Eye Tracking reenabled.");
+        }
+
+        if (MLEyes.IsStarted) {
+            RaycastHit rayHit;
+            heading = MLEyes.FixationPoint - TargetTransform.position;
+
+            if (Physics.Raycast(TargetTransform.position, heading, out rayHit, 10.0f)) {
+
+                if (rayHit.transform.position == gameObject.transform.position) {
+                    meshRenderer.material = FocusedMaterial;
+
+                    if (DetailObjectToOpen != null && !isDetailOpen) {
+                        var heading = TargetTransform.position + TargetTransform.forward * 3;
+                        heading.x = 0;
+                        heading.y = 0;
+
+                        DetailObjectToOpen.transform.position = heading;
+                        DetailObjectToOpen.SetActive(true);
+                        isDetailOpen = true;
+                    }
+                } else if (isDetailOpen && rayHit.transform.position != DetailObjectToOpen.transform.position) {
+                    DetailObjectToOpen.SetActive(false);
+                    isDetailOpen = false;
+                    meshRenderer.material = NonFocusedMaterial;
+                } else {
+                    meshRenderer.material = NonFocusedMaterial;
+                }
+            } else {
+                meshRenderer.material = NonFocusedMaterial;
+            }
+        }
+    }
+}
