5bc81d6bf0f6a5953d199d5b697edf6498415e31
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-05-07 16:48:29 +0200

    improve eye tracking.

--- NotificationBadge/Assets/Scripts/EyeTracking.cs
+++ NotificationBadge/Assets/Scripts/EyeTracking.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.XR.MagicLeap;
 
-public class EyeTracking : MonoBehaviour {
+public class EyeTracking : MonoBehaviour
+{
     public Material FocusedMaterial, NonFocusedMaterial;
     public GameObject DetailObjectToOpen;
 
     [Tooltip("Specifies the target we will orient to. If no target is specified, the main camera will be used.")]
     private Transform targetTransform;
-    public Transform TargetTransform {
+
+    public Transform TargetTransform
+    {
         get { return targetTransform; }
         set { targetTransform = value; }
     }
@@ -15,10 +18,13 @@ public class EyeTracking : MonoBehaviour {
     private Vector3 heading;
     private MeshRenderer meshRenderer;
     private bool isDetailOpen;
-    
+    private int missedCounter;
+
     // Start is called before the first frame update
-    void Start() {
-        if (TargetTransform == null) {
+    void Start()
+    {
+        if (TargetTransform == null)
+        {
             TargetTransform = CameraCache.Main.transform;
         }
 
@@ -26,56 +32,73 @@ public class EyeTracking : MonoBehaviour {
         meshRenderer = gameObject.GetComponent<MeshRenderer>();
     }
 
-    void Awake() {
-        if (DetailObjectToOpen != null) {
+    void Awake()
+    {
+        if (DetailObjectToOpen != null)
+        {
             DetailObjectToOpen.SetActive(false);
         }
     }
 
-    private void OnDisable() {
+    private void OnDisable()
+    {
         Debug.Log("Eye Tracking Disabled");
         MLEyes.Stop();
     }
 
     // Update is called once per frame
-    void Update() {
-        if (!MLEyes.IsStarted) {
+    void Update()
+    {
+        if (!MLEyes.IsStarted)
+        {
             MLEyes.Start();
             Debug.Log("Eye Tracking reenabled.");
         }
 
-        if (MLEyes.IsStarted) {
+        if (MLEyes.IsStarted)
+        {
             RaycastHit rayHit;
             heading = MLEyes.FixationPoint - TargetTransform.position;
 
-            if (Physics.Raycast(TargetTransform.position, heading, out rayHit, 10.0f)) {
+            if (Physics.Raycast(TargetTransform.position, heading, out rayHit, 10.0f))
+            {
+                missedCounter = 0;
 
-                if (rayHit.transform.position == gameObject.transform.position) {
+                if (rayHit.collider.gameObject == gameObject)
+                {
                     meshRenderer.material = FocusedMaterial;
 
-                    if (DetailObjectToOpen != null && !isDetailOpen) {
-                        var heading = TargetTransform.position + TargetTransform.forward * 3;
+                    if (DetailObjectToOpen != null && !isDetailOpen)
+                    {
+                        var heading = TargetTransform.position + TargetTransform.forward * 2;
 
                         DetailObjectToOpen.transform.position = heading;
                         DetailObjectToOpen.SetActive(true);
                         isDetailOpen = true;
                     }
                 }
-                else if (isDetailOpen && rayHit.transform.position == DetailObjectToOpen.transform.position)
+                else if (rayHit.collider.gameObject == DetailObjectToOpen || rayHit.collider.gameObject.tag == "ReadingArea")
                 {
+                    missedCounter = 0;
+                    meshRenderer.material = NonFocusedMaterial;
+                } 
+                else {
+                    DetailObjectToOpen.SetActive(false);
+                    isDetailOpen = false;
+                    missedCounter = 0;
                     meshRenderer.material = NonFocusedMaterial;
                 }
-                else
+            }
+            else
+            {
+                missedCounter++;
+
+                if (missedCounter >= 8)
                 {
                     DetailObjectToOpen.SetActive(false);
                     isDetailOpen = false;
                     meshRenderer.material = NonFocusedMaterial;
                 }
-            } else {
-                DetailObjectToOpen.SetActive(false);
-
-                isDetailOpen = false;
-                meshRenderer.material = NonFocusedMaterial;
             }
         }
     }
